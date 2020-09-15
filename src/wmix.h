eaacdda5a2c86c0749e5f6ee098e61b4d719aedd
WWX <593805475@qq.com>
2020-09-15 09:51:53 +0800

    aec回声消除建议在单声道8000Hz下使用

--- src/wmix.h
+++ src/wmix.h
@@ -58,8 +58,9 @@
  *    1.频率分 8000,16000,32000,48000 和 11025,22050,44100 两个系列,
  *    有些硬件由于时钟配置原因,使用不适配的频率可能导致录播音变声(听起来变了个人);
  *    2.过高的频率和44100系列频率不被webrtc支持,具体支持情况看readme.
+ *    3.aec回声消除cpu占用过高(即使树莓派P4B),建议在单声道8000Hz下使用
  */
-#define WMIX_CHANNELS 2
+#define WMIX_CHANNELS 1
 #define WMIX_SAMPLE 16
 #define WMIX_FREQ 8000
 
